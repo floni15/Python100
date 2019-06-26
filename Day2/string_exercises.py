3cceda0be31d6d3e87c43df693ce291f6ba0dc7c
floni15 <floni@poczta.fm>
2019-06-26 20:24:06 +0200

    initial commit

---
+++ Day2/string_exercises.py
@@ -0,0 +1,15 @@
+message = "Witaj, Jonnie Snow"
+print(message)
+
+Imie = "Michał"
+Nazwisko = "Łukasz"
+
+print(f"Witaj, {Imie} {Nazwisko}")
+
+print(len(Imie))
+
+print(f"Witaj, {Imie}     {Nazwisko}")
+print(90 - 180)
+print(abs(90 - 180))
+
+# komentarz
